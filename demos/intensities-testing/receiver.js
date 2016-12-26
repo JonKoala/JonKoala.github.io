a3fb05cced000476de1a69fa4ea5fab25e7dd19c
Victor Lemos <thevictor89@gmail.com>
2016-12-26 15:44:25 -0200

    adding message display for the intensities testing project

--- demos/intensities-testing/receiver.js
+++ demos/intensities-testing/receiver.js
@@ -11,11 +11,19 @@ function animationLoop() {
   var str = '';
   for (index in intensities)
     str += frequencies[index] + 'Hz -> ' + intensities[index] + 'db<br />';
+  str += '<br />message: ' + message;
 
   document.getElementById('value').innerHTML = str;
 
   setTimeout(animationLoop, 100);
 }
 
+function onChangeMessage(m) {
+  message = m;
+}
+
+var message;
+
 var receiver = new Receiver(onReceiverReady, function(e) {console.log(e)});
 receiver.initialize([18600, 18800, 19000, 19200, 19400, 19600, 19800, 20000], 20200, 20400);
+receiver.onChangeMessage = onChangeMessage
