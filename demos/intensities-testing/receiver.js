25a7728ed7868685d3fd113237cc6ff59e8815cc
Victor Lemos <thevictor89@gmail.com>
2016-12-26 15:18:56 -0200

    adding intensities testing demo

---
+++ demos/intensities-testing/receiver.js
@@ -0,0 +1,21 @@
+
+function onReceiverReady() {
+  animationLoop();
+}
+
+function animationLoop() {
+  
+  var frequencies = [18600, 18800, 19000, 19200, 19400, 19600, 19800, 20000, 20200];
+
+  var intensities = receiver.getIntensityValues(frequencies);
+  var str = '';
+  for (index in intensities)
+    str += frequencies[index] + 'Hz -> ' + intensities[index] + 'db<br />';
+
+  document.getElementById('value').innerHTML = str;
+
+  setTimeout(animationLoop, 100);
+}
+
+var receiver = new Receiver(onReceiverReady, function(e) {console.log(e)});
+receiver.initialize([18600, 18800, 19000, 19200, 19400, 19600, 19800, 20000], 20200, 20400);
